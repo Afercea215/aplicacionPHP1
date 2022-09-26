98ab28be1012944c44eddaae21a5937514a78947
afercea215 <afercea215@g.educaand.es>
2022-09-26 09:42:19 +0200

    V1.5 Verifica si son numeros.

--- app.php
+++ app.php
@@ -2,9 +2,12 @@
     if (!empty($_GET['a'])&&!empty($_GET['b'])) {
         $n1 = $_GET['a'];
         $n2 = $_GET['b'];
-        
-        $result = $n1 + $n2;
-        echo $result;
+        if (is_numeric($n1)&&is_numeric($n2)) {
+            $result = $n1 + $n2;
+            echo $result;   
+        }else {
+            echo "Los datos deben ser numeros";
+        }  
     }else {
         echo "Debe de rellenar todos los datos";
     }
